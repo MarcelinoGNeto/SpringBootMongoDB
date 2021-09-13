917fbe2b1cc191ce66b72c87351e5793964dce95
MarcelinoGNeto <marcelino.gneto@gmail.com>
2021-09-13 18:36:49 -0300

    Projeto Criado

---
+++ src/test/java/com/marcelino/workshopmongo/WorkshopmongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelino.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
